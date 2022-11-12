e1dd1a4c401d8b97d697a9155c938814f910cd9d
Krtucho <Krtucho98@gmail.com>
2022-11-12 15:44:49 -0500

    boolean_model

--- boolean_model.py
+++ boolean_model.py
@@ -4,8 +4,19 @@ class Boolean_model:
     def __init__(self):
         pass
 
-    def load_documents(self, documents)-> dict:
-        pass
+    def load_documents(self, documents)-> dict: # Asumo que documents es un array de documentos de tipo doc
+        tokens_list = {}
+
+        for doc in documents:
+            tokens = set(documents.text)
+            for token in tokens:
+                if not tokens_list.__contains__(token):
+                    tokens_list[token] = [doc]
+                else:
+                    tokens_list[token].append(doc)
+                    
+        return tokens_list
+
 
     def load_query(self, query):
         pass
