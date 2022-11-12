f9342c1ca57aeeb86015bab9d91ab65301ed2eb7
Krtucho <Krtucho98@gmail.com>
2022-11-12 15:35:14 -0500

    boolean_model

--- boolean_model.py
+++ boolean_model.py
@@ -4,6 +4,12 @@ class Boolean_model:
     def __init__(self):
         pass
 
+    def load_documents(self, documents)-> dict:
+        pass
+
+    def load_query(self, query):
+        pass
+
     def similitud():
         pass
         
