8cb8d7c570b1b6130fc491d76f1118407a27b530
Daniela <91924071+Daroce1012@users.noreply.github.com>
2022-11-14 13:01:28 -0600

    update

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from parse import All_Dir_Doc,Create_Data
 def main():
 
     dir_docs,ids = All_Dir_Doc()  # contiene las direcciones de todos los doc y su id la pos en que estos se encuentran en estos es la misma que en data
-    data = Create_Data(dir_docs)
+    data = Create_Data(dir_docs,ids)
     modelo=Boolean_model(data)
         
     while(True):
@@ -17,7 +17,12 @@ def main():
             break
         
         query = Clear_Query(query_text)
-        print([doc.title for doc in modelo.similitud(query, modelo.tokens_list)])
+        query = modelo.load_query(query)
+        print(query)
+        titles = [doc.title +"\n" for doc in modelo.similitud(query, modelo.tokens_list)]
+        
+        for title in titles:
+            print(title)
 
     
 
