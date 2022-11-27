c11539c707bc0cf7014936e41a2c98f97a758bdf
Daniela <91924071+Daroce1012@users.noreply.github.com>
2022-11-27 09:15:02 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -21,7 +21,23 @@ def main():
         print(query)
         titles = [doc.title for doc in modelo.similitud(query, modelo.tokens_list)]
         
-        for title in titles:
-            print(title)
+        # for title in titles:
+        #     print(title)
+        print(titles)
+    
+
+    # print("hola")
+    # # documents=[["amarillo","verde","rojo"],["rojo"],["azul","violeta","amarillo"],["blanco","negro"],["blanco","rojo"]]
+    # docu1=Doc("Hola",["amarillo","verde","rojo"])
+    # docu2=Doc("Hola1", ["rojo"])
+    # docu3=Doc("Hola2",["azul","violeta","amarillo"])
+    # docu4=Doc("Hola3",["blanco","negro"])
+    # docu5=Doc("Hola4", ["blanco","rojo"])
+    # documentos=[docu1,docu2,docu3,docu4,docu5]
+    # print(documentos)
+    #query=["not","amarillo","and","rojo","or","not","blanco"]
+    #print(query)
+    #modelo=Boolean_model(documentos)
+    #print([doc.title for doc in modelo.similitud(query, modelo.load_documents(documentos))])
     
 main()
