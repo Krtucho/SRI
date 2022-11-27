0558e7a187399f129bb41fd2771adde50e55af6d
Daniela <91924071+Daroce1012@users.noreply.github.com>
2022-11-27 09:14:11 -0600

    Add files via upload

--- parse.py
+++ parse.py
@@ -23,10 +23,10 @@ def All_Dir_Doc():
 
 def Clear(text):
     new_text = text.lower()
-    
+    #new_text = re.sub(r"\s+", "", text)
     tokens = nltk.word_tokenize(new_text)
     #Unique Tokens (Solo deja un conj de tokens)
-
+    #tokens=list(set(tokens))
     
     # Remove Special characters (Elimina los caracteres especiales)
     removetable=str.maketrans("", "", "!@#$%^&*()_=-\|][:';:,<.>/?`~") 
@@ -35,6 +35,7 @@ def Clear(text):
     #Remove the stopwords
     stop_words = stopwords.words('english')
     tokens = list(set(tokens)-set(stop_words))
+    #tokens = [i for i in tokens if i not in stop_words]
 
     return list(filter(None, tokens))
     
@@ -46,23 +47,26 @@ def Read(archive):
         while(line):
             line = archive.readline()
             if(count == 1):
-               title = line[9:-1]
+               #title = line[9:-1]
+               text = line[9:-1]
             elif line != "\n":
                 text = text+""+ line[:-1] 
             count = count+1
-        return title,text
+        #return title,text
+        return text
 
 
-def Create_Data(dir_docs):
+def Create_Data(dir_docs,id_doc):
     data = [] 
     id = 0
     for dir in dir_docs:
         archive = open(dir)
-        title,text = Read(archive)
+        #title,text = Read(archive)
+        text = Read(archive)
         #print(text)
-        title= Clear(title)
+        #title= Clear(title)
         term = Clear(text)
-
+        title = str(id_doc[id])
         doc = Doc(id,title,term)
         data.append(doc)
         id = id+1
@@ -70,4 +74,19 @@ def Create_Data(dir_docs):
         archive.close()
         #break
     return data   
+
+
+
+#dir_docs,ids = All_Dir_Doc()  # contiene las direcciones de todos los doc y su id la pos en que estos se encuentran en estos es la misma que en data
+#data = Create_Data(dir_docs)
+
+#print(data[0].term)
+#print(data[0].title)  
+
+# for i in data:
+#     print(i.term)
+#     print(i.title)  
+
+   
+
    
