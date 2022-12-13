da107c837c43f44178b15f81be03b9ce32ac769f
bel5599 <95463994+bel5599@users.noreply.github.com>
2022-12-13 15:34:59 -0800

    modelo vectorial un poco mas eficiente que la anterior version 12 seg

--- main.py
+++ main.py
@@ -1,34 +1,21 @@
-<<<<<<< Updated upstream
-from boolean_model import Boolean_model
-from documents import Doc
-
-def main():
-    print("hola")
-    # documents=[["amarillo","verde","rojo"],["rojo"],["azul","violeta","amarillo"],["blanco","negro"],["blanco","rojo"]]
-    docu1=Doc("Hola",["amarillo","verde","rojo"])
-    docu2=Doc("Hola1", ["rojo"])
-    docu3=Doc("Hola2",["azul","violeta","amarillo"])
-    docu4=Doc("Hola3",["blanco","negro"])
-    docu5=Doc("Hola4", ["blanco","rojo"])
-    documentos=[docu1,docu2,docu3,docu4,docu5]
-    print(documentos)
-    query=["not","amarillo","and","rojo","or","not","blanco"]
-    print(query)
-    modelo=Boolean_model(documentos)
-    print([doc.title for doc in modelo.similitud(query, modelo.load_documents(documentos))])
-
-main()
-=======
 from boolean_model import Boolean_model
 from vector_model import Vector_Model
 from query import Clear_Query
 from parse import All_Dir_Doc,Create_Data
 from documents import Doc
+import time
 
 def main():
 
     dir_docs,ids = All_Dir_Doc()  # contiene las direcciones de todos los doc y su id la pos en que estos se encuentran en estos es la misma que en data
+    
+    inicio=time.time()
+    
     data = Create_Data(dir_docs,ids)
+    
+    fin=time.time()
+    print("El tiempo en crear la data")
+    print(fin-inicio)
     # data=[]
     # doc1= Doc("doc1","hola",["leon","leon","leon"])
     # data.append(doc1)
@@ -47,37 +34,57 @@ def main():
         
         print("Welcome, Please enter your query")
         query_text = input()
+
     # query_text="shdkjfbdj kjdj"
     # query_text="leon zorro nutria"
     # query_text="lion and tiger tiger tiger be only you and never forget it forget it 2017"
+    # query_text="forget 2017 tiger "
         
         if(query_text == "exit"):
             break
         
     query = Clear_Query(query_text)
-        # query = modelo.load_query(query)
+    query = modelo.load_query(query)
+
     print(query)
+
+
     # print(modelo.load_documents(data))
-    print(modelo.freq_normal(query))
+    # print(modelo.freq_normal(query))
 
-    print("idf")
-    print(modelo.idf(query))
+    # print("idf")
+    # print(modelo.idf(query))
 
-    print("peso de los documentos")
-    w_i_j=modelo.docs_weights(query)
-    for term,doc in w_i_j:
-        print(term, doc.id, w_i_j[(term,doc)])
+    # print("peso de los documentos")
+    # w_i_j=modelo.docs_weights(query)
+    # for term,doc in w_i_j:
+    #     print(term, doc.id, w_i_j[(term,doc)])
         # print(doc.id)
     # print(modelo.docs_weights(query))
 
-    print("peso de la consulta")
-    print(modelo.query_weights(query))
+    # print("peso de la consulta")
+    # print(modelo.query_weights(query))
     
-    print("frecuencia normalizada de la consulta")
-    print(modelo.freq_normal_q(query))
+    # print("frecuencia normalizada de la consulta")
+    # print(modelo.freq_normal_q(query))
 
-    print("similitud entre un doc y la consulta")
-    print(modelo.similitud(query, doc5))
+    #TIMER: ESO SE PUEDE COMENTAR
+    inicio_general=time.time()
+
+    for i,doc in enumerate(modelo.documents):
+        inicio=time.time()
+
+        print(modelo.similitud(query, doc))
+       
+
+        fin=time.time()
+        print(i)
+        print("El tiempo que se demoro")
+        print(fin-inicio)
+        
+    final=time.time()
+    print(final-inicio_general)
+    
         
         # for title in titles:
         #     print(title)
@@ -100,4 +107,3 @@ def main():
     
 main()
 
->>>>>>> Stashed changes
--- vector_model.py
+++ vector_model.py
@@ -6,6 +6,8 @@ class Vector_Model:
     def __init__(self,documents):
         self.documents=documents
         self.tokens_list = self.load_documents(documents)
+        self.q_weights=None
+        self.doc_weights=None
 
     def load_documents(self, documents)-> dict: # Asumo que documents es un array de documentos de tipo doc
         tokens_list = {}
@@ -20,27 +22,9 @@ class Vector_Model:
 
         return tokens_list
 
-    #metodo la frecuencia que tiene un termino en un documento
-    # def freq_term_docs(self, term, doc):
-    #     count=0
-    #     for item in doc:
-    #         if item==term:
-    #             count+=1
-    #     return count
-
-    # def freq_count(self,term,doc):
-    #     count=0
-    #     for item in doc:
-    #         if item
-
-#metodo que calcula la frecuencia de los terminos de la query en los docs
-    # def freq(self, query, token_list):
-    #     freq_term_i_docs_j={}
-
-    #     for term in query:
-    #         for item_doc in token_list[term]:
-    #             freq_term_i_docs_j[(term,item_doc)]= item_doc.term_frec[term]
-    #     return freq_term_i_docs_j
+    def load_query(self,query):
+        self.doc_weights=self.docs_weights(query)
+        self.q_weights=self.query_weights(query)
 
 #metodo que calcula la frecuencia normalizada de la consulta
     def freq_normal_q(self, query):
@@ -57,17 +41,6 @@ class Vector_Model:
             term_freq[f] /= max_freq  
         return term_freq
 
-# #metodo que calcula la frecuencia max que tiene de todos los terminos en un doc
-#     def max_term_doc(self, freq_term_i_docs_j):
-#         max_docs={}
-#         for term_doc, freq in freq_term_i_docs_j.items():
-
-#             if not max_docs.__contains__(term_doc[1]):
-#                 max_docs[term_doc[1]]=freq
-
-#             elif max_docs[term_doc[1]]<freq:
-#                 max_docs[term_doc[1]]=freq
-#         return max_docs
 
 #metodo que calcula la frecuencia normalizada de los documentos
     def freq_normal(self, query):
@@ -80,13 +53,6 @@ class Vector_Model:
                 else:
                     freq_normal_term_docs[(term,doc)] = 0
 
-        # for term in query:
-        #     if term in token_list:
-        #         for item_doc in token_list[term]:
-        #             freq_normal_term_docs[(term,item_doc)]= item_doc.term_frec[term]
-        #     else:
-        #         for doc in self.documents:
-        #             freq_normal_term_docs[(term,doc)]=0
         return freq_normal_term_docs
 
     def idf(self,query):
@@ -102,8 +68,8 @@ class Vector_Model:
 
     def similitud(self, query, doc):
 
-        q_weights = self.query_weights(query)
-        doc_weights = self.docs_weights(query)
+        q_weights = self.q_weights
+        doc_weights = self.doc_weights
 
         sum_weights_total=0
         sum_cuadrado_doc_weights=0
@@ -118,6 +84,9 @@ class Vector_Model:
                 sum_weights_total += 0
                 sum_cuadrado_doc_weights += 0
                 sum_cuadrado_q_weights += pow(q_weights[term], 2)
+        if sum_cuadrado_doc_weights==0 or sum_cuadrado_q_weights==0:
+            return 0
+            
         sim_doc_q=sum_weights_total / (math.sqrt(sum_cuadrado_doc_weights) * math.sqrt(sum_cuadrado_q_weights))
         return sim_doc_q
 
@@ -130,10 +99,6 @@ class Vector_Model:
         doc_weights={}
         for term,doc in freq_nor.keys():
             doc_weights[(term,doc)]=freq_nor[(term,doc)] * idf_i[term]
-        # for term in query:
-        #     for item_doc in token_list[term]:
-        #         doc_weights[(term,item_doc)]=freq_nor[(term,item_doc)] * idf_i[term]
-
         return doc_weights
 
         
@@ -147,7 +112,7 @@ class Vector_Model:
         q_weights={}
         for term in query:
             q_weights[term]=( 0.5 + (1 - 0.5) * freq_nor_q[term] ) * idf_i[term]
-
+        
         return q_weights
 
 
